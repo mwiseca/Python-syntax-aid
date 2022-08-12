c99251d85f5e1f1e3451840626742bfc1a858076
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2022-08-12 08:28:38 -0400

    Add files via upload

---
+++ classes  with init.py
@@ -0,0 +1,53 @@
+eng = {
+      "h": "class ",
+      "i": ":",
+      "j": "    def __init__(self," ,
+      "k": "):",
+      "l": "self.",
+      "m": "=",
+      "n": " ",
+      "o": "        ",
+}      
+while True:    
+    print("#Enter a class")
+    text = input("#")
+    if text == "m":
+        break
+    print("#List attributes.")
+    te = input("#")
+    if te == "m":
+        break
+    print("#Assign values. One then press enter.")    
+    v = input("#1")
+    v1 = input("#2")
+    v2 = input("#3")
+    v3 = input("#4")
+    v4 = input("#5")
+    v5 = input("#6")
+    v6 =input("#7")
+    v7 = input("#8")
+    print(eng["h"] + text + eng["i"] )
+    print(eng["j"] + (te) + eng["k"])
+    print(eng["o"] + eng["l"] +  v  +  eng["n"] + eng["m"] + eng["n"] + v )
+    print(eng["o"] + eng["l"] +  v1  +  eng["n"] + eng["m"] + eng["n"] + v1)
+    print(eng["o"] + eng["l"] +  v2  +  eng["n"] + eng["m"] + eng["n"] + v2)
+    print(eng["o"] + eng["l"] +  v3  +  eng["n"] + eng["m"] + eng["n"] + v3)
+    print(eng["o"] + eng["l"] +  v4  +  eng["n"] + eng["m"] + eng["n"] + v4)    
+    print(eng["o"] + eng["l"] +  v5  +  eng["n"] + eng["m"] + eng["n"] + v5)
+    print(eng["o"] + eng["l"] +  v6  +  eng["n"] + eng["m"] + eng["n"] + v6)
+    print(eng["o"] + eng["l"] +  v7  +  eng["n"] + eng["m"] + eng["n"] + v7)    
+     
+       
+    
+    
+    
+    
+     
+        
+    
+            
+    
+   
+
+    
+    
