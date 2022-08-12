52e0cf2831192811a4be710f769a72ac02eb0e03
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2022-08-12 08:42:42 -0400

    Update print string with classes.py

--- print string with classes.py
+++ print string with classes.py
@@ -75,6 +75,15 @@ while True:
             te = input("#")
             if te == "m":
                 break
+            print("#Assign values. One then press enter.")    
+            v = input("#1")
+            v1 = input("#2")
+            v2 = input("#3")
+            v3 = input("#4")
+            v4 = input("#5")
+            v5 = input("#6")
+            v6 =input("#7")
+            v7 = input("#8")
             print(eng["h"] + text + eng["i"] )
             print(eng["j"] + (te) + eng["k"])
             print(eng["o"] + eng["l"] +  v  +  eng["n"] + eng["m"] + eng["n"] + v )
