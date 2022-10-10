e7e15af7255c09aa06b4286a5c42e33bf8dc9c84
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2022-10-09 23:38:20 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,45 @@
+
+ 
+    
+eng = {
+    "a": " = ",
+    "b": '"',
+    "c": 'input("',
+    "d": '")',
+    "e": " "
+}
+while True:
+    print("Enter a name for your variable.")
+    n = input("#")
+    if n == "x":
+        break
+    print("#If the value is a string hit s  i for user input otherwise enter. ")
+    vs = input("#")
+    v = input("#Enter a value. ")
+    if vs == "s":
+        print(n + eng["a"] + eng["b"] + v + eng["b"])
+    elif vs == "i":
+        print(n + eng["a"] + eng["c"] + v +  eng["e"] + eng["d"])
+    else:
+        print(n +  eng["a"] + v)
+
+
+
+
+
+
+
+
+
+
+
+        
+
+    
+    
+    
+        
+        
+        
+    
+          
