e89f450ba4e5890142067ad96bc5ca890372d59d
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2026-07-12 09:04:41 -0400

    Update choice prompt for string options
    Removed option for 'a for one' in the choice prompt.

--- python syntax aid.c
+++ python syntax aid.c
@@ -26,7 +26,7 @@ void checkInput() {
 }
 
 void choice() {
-    printf("#For strings enter a for one b for two c for three quotes. Enter x to exit.\n");
+    printf("#For strings enter b for two c for three quotes.\n");
     printf("#Enter cl for classes. Enter i for if statements. Enter e for elif.\n");
     printf("#Enter v for variables and user input option.\n");
     printf("#Enter ch for choices.\n");
